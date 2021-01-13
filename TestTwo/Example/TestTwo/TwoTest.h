7c486a4e85250016d1ec9e0871051851ef199f1c
vincen <869270468@qq.com>
2021-01-13 11:14:27 +0800

    no message

---
+++ TestTwo/Example/TestTwo/TwoTest.h
@@ -0,0 +1,17 @@
+//
+//  TwoTest.h
+//  TestTwo_Example
+//
+//  Created by Liao PanPan on 2021/1/13.
+//  Copyright © 2021 L-vinCent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TwoTest : NSObject
++(void)log;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestTwo/Example/TestTwo/TwoTest.m
@@ -0,0 +1,15 @@
+//
+//  TwoTest.m
+//  TestTwo_Example
+//
+//  Created by Liao PanPan on 2021/1/13.
+//  Copyright © 2021 L-vinCent. All rights reserved.
+//
+
+#import "TwoTest.h"
+
+@implementation TwoTest
++(void)log{
+    NSLog(@"smart");
+}
+@end
