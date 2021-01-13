ec6bfd9c427234c1a66d57036be98c082fda4b33
vincen <869270468@qq.com>
2021-01-13 11:10:28 +0800

    no message

---
+++ TestTwo/Example/TestTwo/PPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PPAppDelegate.h
+//  TestTwo
+//
+//  Created by L-vinCent on 01/13/2021.
+//  Copyright (c) 2021 L-vinCent. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestTwo/Example/TestTwo/PPViewController.m
@@ -0,0 +1,29 @@
+//
+//  PPViewController.m
+//  TestTwo
+//
+//  Created by L-vinCent on 01/13/2021.
+//  Copyright (c) 2021 L-vinCent. All rights reserved.
+//
+
+#import "PPViewController.h"
+
+@interface PPViewController ()
+
+@end
+
+@implementation PPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
